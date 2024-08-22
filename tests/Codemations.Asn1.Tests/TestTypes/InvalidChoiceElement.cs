9728266a0664364dd660bced7ab3d75a05916030
Arkadiusz Prochwicz <aprochwicz@gmail.com>
2024-08-22 12:38:37 +0200

    Add choice converter test

---
+++ tests/Codemations.Asn1.Tests/TestTypes/InvalidChoiceElement.cs
@@ -0,0 +1,12 @@
+﻿namespace Codemations.Asn1.Tests
+{
+    [AsnChoice]
+    public class InvalidChoiceElement
+    {
+        [AsnElement(0x80)]
+        public int? IntegerElement { get; set; }
+
+        [AsnElement(0x80)]
+        public bool? BooleanElement { get; set; }
+    }
+}
