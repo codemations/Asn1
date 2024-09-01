5c9a7ff7885874e43a979d162118bf1d445f6efe
Arkadiusz Prochwicz <aprochwicz@gmail.com>
2024-09-01 12:42:08 +0200

    Throw proper exceptions

--- src/Codemations.Asn1/Converters/AsnPolymorphicChoiceConverter.cs
+++ src/Codemations.Asn1/Converters/AsnPolymorphicChoiceConverter.cs
@@ -1,5 +1,4 @@
 ﻿using Codemations.Asn1.Attributes;
-using Codemations.Asn1.Extensions;
 using System;
 using System.Formats.Asn1;
 using System.Linq;
@@ -18,18 +17,24 @@ internal class AsnPolymorphicChoiceConverter : AsnSequenceConverter
     {
         var choiceTag = reader.PeekTag();
 
-        var selectedChoice = GetChoices(type).FirstOrDefault(x => x.Tag == choiceTag) ?? throw new Exception();
+        var selectedChoice = GetChoice(type, attribute => attribute.Tag == choiceTag)
+            ?? throw new AsnConversionException($"No matching choice found for ASN.1 tag {choiceTag} in type '{type.FullName}'.");
 
         return base.Read(reader, choiceTag, selectedChoice.ChoiceType, serializer);
     }
 
     public override void Write(AsnWriter writer, Asn1Tag? tag, object value, AsnSerializer serializer)
     {
-        var baseType = value.GetType().BaseType!;
-
-        var selectedChoice = GetChoices(baseType).FirstOrDefault(x => x.ChoiceType == value.GetType()) ?? throw new Exception();
+        var selectedChoice = GetChoice(value.GetType(), x => x.ChoiceType == value.GetType())
+            ?? throw new AsnConversionException($"No matching ASN.1 choice found for the type '{value.GetType().FullName}'.");
 
         base.Write(writer, selectedChoice.Tag, value, serializer);
+    }
+
+    private static AsnPolymorphicChoiceAttribute? GetChoice(Type type, Predicate<AsnPolymorphicChoiceAttribute> predicate)
+    {
+        var choices = GetChoices(type);
+        return Array.Find(choices, predicate);
     }
 
     private static AsnPolymorphicChoiceAttribute[] GetChoices(Type type)
